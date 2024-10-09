5e39ffe1fea336b9068b129e098a9b5c3b4cc8fe
William Burdick <williamfburdick@gmail.com>
2024-10-09 16:41:18 -0400

    added sprite rendering to the game class

--- OpenGL-Game/src/Game.cpp
+++ OpenGL-Game/src/Game.cpp
@@ -1,15 +1,31 @@
 #include "Game.h"
+#include "ResourceManager.h"
+#include "SpriteRenderer.h"
 
-Game::Game(unsigned int width, unsigned int height)
+SpriteRenderer* Renderer;
+
+Game::Game(unsigned int width, unsigned int height):
+	State(GAME_ACTIVE), Keys(), Width(width), Height(height)
 {
 }
 
 Game::~Game()
 {
+	delete Renderer;
 }
 
 void Game::Init()
 {
+	ResourceManager::LoadShader("shaders/sprite.vert", "shaders/sprite.frag", nullptr, "sprite");
+	// configure shader
+	glm::mat4 projection = glm::ortho(0.0f, static_cast<float>(Width), static_cast<float>(Height), 0.0f, -1.0f, 1.0f);
+	ResourceManager::GetShader("sprite").Use().SetInteger("image", 0);
+	ResourceManager::GetShader("sprite").Use().SetMatrix4("projection", projection);
+
+	Shader shader = ResourceManager::GetShader("sprite");
+	Renderer = new SpriteRenderer(shader);
+
+	ResourceManager::LoadTexture("textures/awesomeface.png", true, "face");
 }
 
 void Game::ProcessInput(float dt)
@@ -22,4 +38,6 @@ void Game::Update(float dt)
 
 void Game::Render()
 {
+	Texture2D face = ResourceManager::GetTexture("face");
+	Renderer->DrawSprite(face, glm::vec2(200.0f, 200.0f), glm::vec2(300.0f, 400.0f), 45.0f, glm::vec3(0.0f, 1.0f, 0.0f));
 }
